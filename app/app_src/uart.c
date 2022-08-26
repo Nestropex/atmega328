156682d00d87126bb9eea2b850fe58cda94d8a4e
Bonnski <bon@nestromo.com>
2022-08-26 19:24:31 +0200

    more clean up

--- app/app_src/uart.c
+++ app/app_src/uart.c
@@ -7,55 +7,69 @@
 #include "datatypes.h"
 
 
-
-
+#define INIT_IN_SYNCH_MODE 0x80u
+#define BAUDRATE_9600      103u
+#define ENABLE_TRANSMIT    (1 << TXEN0 | 1 << RXEN0)
+#define STOP_BITS_2        (1 << USBS0) 
+#define FRAME_SIZE_8BIT    (1<<UCSZ00 | 1<<UCSZ01)
 
 void uart_init(void)
 {
-    UCSR0C = 0x80u; // Init in synchronous mode
-    UCSR0C |= (1<<UCSZ00 | 1<<UCSZ01); // 8bit transmit size
-    UCSR0C |= 1 << USBS0 ;  // 2 Stop bit
-    UCSR0B |= (1 << TXEN0 | 1 << RXEN0); //transmit enable
-    UBRR0L = 103; // 9600 baud @ 16 MHz
+    UCSR0C  = INIT_IN_SYNCH_MODE;
+    UCSR0C |= FRAME_SIZE_8BIT; 
+    UCSR0C |= STOP_BITS_2;              
+    UCSR0B |= ENABLE_TRANSMIT;
+    UBRR0L  = BAUDRATE_9600;              
 
 }
 
 uint8_t uart_str_transmit(uint8_t *str)
 {
-    do
+    uint8_t count = 0u;  
+
+    if (str != NULL_PTR)
     {
-        while(!(UCSR0A & (1 << UDRE0)))
+        
+
+        do
         {
+            while(!(UCSR0A & (1 << UDRE0)))
+            {
 
-        }
+            }
 
-        UDR0 = *str;
-        str++;
-    }while(*str != '\0');
+            UDR0 = *str;
+            str++;
+            count++;
 
-    return 0;
+        }while(*str != '\0');
+    }
+
+    return count;
 }
 
+// This function is taken and modified from printNumber()
+// to find in Arduino\hardware\arduino\avr\cores\arduino\print.ccp
 void uart_nmb_transmit(unsigned long n, uint8_t base)
 {
-  uint8_t buf[8 * sizeof(long) + 1]; // Assumes 8-bit chars plus zero byte.
+  uint8_t buf[8 * sizeof(long) + 1]; 
   uint8_t *str = &buf[sizeof(buf) - 1];
   uint8_t count = 0u;
 
   *str = '\0';
 
-  // prevent crash if called with base == 1
-  if (base < 2) base = 10;
+  if (base > 1u)
+  {
+    do {
+        char c = n % base;
+        n /= base;
 
-  do {
-    char c = n % base;
-    n /= base;
+        *--str = c < 10 ? c + '0' : c + 'A' - 10; // else means hex numbers are required
+        count++;
+       } while(n);
 
-    *--str = c < 10 ? c + '0' : c + 'A' - 10;
-    count++;
-  } while(n);
-
-  uart_str_transmit(str);
-  uart_str_transmit("\n");
-  
+    uart_str_transmit(str);
+    uart_str_transmit("\n");
+  }
+ 
 }
