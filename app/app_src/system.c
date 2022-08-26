241f09aac3a79fd311106e9ff12570d0dd4b537d
Bonnski <bon@nestromo.com>
2022-08-26 23:28:34 +0200

    implemented error update function

--- app/app_src/system.c
+++ app/app_src/system.c
@@ -8,6 +8,8 @@
 
 #define ERROR_FREQUENCY 10u
 
+static uint8_t stc_error_occurred;
+
 void system_error_handler(uint8_t *error_description)
 {
     uart_str_transmit(error_description);
@@ -15,9 +17,20 @@ void system_error_handler(uint8_t *error_description)
 
     gpio_flash(g_error_out.port, g_error_out.bit, ERROR_FREQUENCY);
 
+    stc_error_occurred = 1u;
 
 }
 
+
+void system_error_update(void)
+{
+    if (stc_error_occurred != 0u)
+    {
+        system_error_handler("An ERROR has occurred atleast once at start up");
+    }
+    
+
+}
 
 
 
--- app/inc/system.h
+++ app/inc/system.h
@@ -9,11 +9,22 @@
  * @brief Brings the system to a defined state in case of error.
  *        The error description from the param will be sent via uart 
  *        and can be read via Serial Monitor. 
- *        Also the configured error pin will be flashed
+ *        Also the configured error pin will be flashed.
+ * 
+ *        This function should be called in case of NULL pointer or switch break statement etc. 
  * 
  * @param error_description a string that holds information where the error occurred
  *                          i.e. module and function name
  */
 void system_error_handler(uint8_t *error_description);
+
+/**
+ * @brief Will check at every call whether an error has occurred. 
+ *        This will prevent that an error is overseen 
+ *        in case it occurred only once at start up. 
+ * 
+ *        This Function should be called periodically, i.e. every loop
+ */
+void system_error_update(void);
 
 #endif
