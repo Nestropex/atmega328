cba04b54728383b86cba186bc855d91bc9ef565c
Bonnski <bon@nestromo.com>
2022-08-29 19:00:02 +0200

    add function comments

--- app/inc/loop.h
+++ app/inc/loop.h
@@ -13,8 +13,24 @@ typedef struct loop {
     uint32_t diff;
 }loop_t;
 
+/**
+ * @brief Fills requested loop object with time since the last execution 
+ *        of this function and number of executions since stat up
+ * 
+ *        Shall be called everywhere where periodic timining must be monitored 
+ * 
+ * @param period Object to fill with requested info
+ */
 void loop_update(loop_t  *period);
 
+/**
+ * @brief Sends looptime and loopcount of requested object via uart
+ * 
+ *        Debug purpose only
+ * 
+ * @param period Info of this object will be send 
+ * @param str Text that shall be visible on serial monitor
+ */
 void loop_print(loop_t *period, uint8_t *str);
 
 
