c99c399e8b43bdc9933097fe8b044e7ad9269cd4
Bonnski <bon@nestromo.com>
2022-09-15 15:48:28 +0200

    both apps can be compiled independently of each other

--- target/coilwind/app.mk
+++ target/coilwind/app.mk
@@ -6,7 +6,7 @@ MAKEFILE_DIR = ../generic
 MCU = ATmega328P
 TARGET = atmega328
 HAL_DIR = ../../atmega328p
-APP_NAME = bldcdriver
+APP_NAME = coilwind
 APP_CFG_DEFINE = $(shell echo $(APP_NAME) | tr a-z A-Z)
 
 DEFINES += -D__AVR_ATmega328P__
