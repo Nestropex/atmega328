fa376a44931081f825d6cb0a911bfa3452e3e7e1
Bonnski <bon@nestromo.com>
2022-09-20 17:55:49 +0200

    forgot to add

--- app/src/signal_out.c
+++ app/src/signal_out.c
@@ -13,7 +13,8 @@
 #include "uart.h"
 #include "timer_32bit.h"
 #include "signal_out.h"
-
+// Allow kernel stuff to keep isr small
+#include <avr/io.h>
 
 //-------Defines-------
 
@@ -55,22 +56,27 @@ static void signal_register(signal_t *channel, uint8_t nmb_of_channels)
     
 }
 
-void signal_rectangle(signal_t *channel)
+void signal_rectangle(signal_t *channel, uint8_t nmb_of_channels)
 {
     if (channel != NULL_PTR)
     {
-        uint32_t timer_freq = timer_get_frequency(SYSTEM_CLK, TIMER_TIMER1_PRESCALER);
-        channel->period_ticks = timer_freq/(2u*(*(channel->frequency)));
+        for (uint8_t i = 0u; i < nmb_of_channels; i++)
+        {
+            uint32_t timer_freq = timer_get_frequency(SYSTEM_CLK, TIMER_TIMER1_PRESCALER);
+            channel[i].period_ticks = timer_freq/(2u*(*(channel[i].frequency)));
         
-        uint16_t local_phase_ticks = 360u/(*(channel->phase));
+            uint16_t local_phase_ticks = 360u/(*(channel[i].phase));
 
-        channel->delay_ticks = channel->period_ticks/(local_phase_ticks/2u);
+            channel[i].delay_ticks = channel[i].period_ticks/(local_phase_ticks/2u);
+        }
+        
+
 
         if (once == 0u)
         {
             uint32_t cur_ticks = timer1_32_get_ticks();
             // Interrupts will occure with a rate 6 times faster than frequency
-            channel->next_hi_event = cur_ticks + 2000u;
+            channel[0].next_hi_event = cur_ticks + 2000u;
             timer_set_compare(Timer1_Comp_A, cur_ticks + 1000);
         }
         
@@ -80,47 +86,39 @@ void signal_rectangle(signal_t *channel)
 
 
 static uint8_t isr_state;
-
+static uint32_t last_event;
+static uint8_t state[NMB_OF_OUTPUTS];
 void signal_timer1_comp_a_isr(void)
 {
-    gpio_write(1,0,1);
+    PORTB |= 0x01u;
     uint32_t cur_ticks = timer1_32_get_ticks();
 
-    for (uint8_t i = 0; i < NMB_OF_OUTPUTS; i++)
+    for (uint8_t i = 0u; i < (NMB_OF_OUTPUTS-0); i++)
     {
-        if (channel_isr[i]->next_hi_event <= cur_ticks)
+        if (((channel_isr[0]->next_hi_event + channel_isr[0]->delay_ticks * i) <= cur_ticks) && (state[i] == 0u))
         {
-            gpio_write(channel_isr[i]->pin_out.port,channel_isr[i]->pin_out.bit, 1u);
-            channel_isr[i]->next_hi_event  = cur_ticks + channel_isr[i]->period_ticks*2;
-
-            if (i == 0u)
-            {
-                channel_isr[1]->next_hi_event  = cur_ticks + channel_isr[1]->delay_ticks;
-                channel_isr[1]->next_low_event = channel_isr[1u]->next_hi_event + channel_isr[1]->period_ticks;
-                channel_isr[2]->next_hi_event  = cur_ticks + channel_isr[2]->delay_ticks*2;
-                channel_isr[2]->next_low_event = channel_isr[2u]->next_hi_event + channel_isr[2]->period_ticks;
-                channel_isr[0]->next_hi_event  = cur_ticks + channel_isr[0]->period_ticks*2;
-                channel_isr[0]->next_low_event = cur_ticks + channel_isr[0]->period_ticks;
-
-            }
-            
-            
+            PORTB |= (1 << channel_isr[i]->pin_out.bit);
+            //gpio_write(channel_isr[i]->pin_out.port,channel_isr[i]->pin_out.bit, 1u);
+            channel_isr[i]->next_low_event = cur_ticks + channel_isr[i]->period_ticks;
+            state[i] = 1u;
         }
     }
 
-    timer_set_compare(Timer1_Comp_A, cur_ticks + 50u);
-    timer_set_compare(Timer1_Comp_B, cur_ticks + 25u);
-    gpio_write(1,0,0);
+    OCR1A = cur_ticks + 50u;
+    OCR1B  =cur_ticks + 25u;
+    PORTB &= 0xfe;
 }
 
 void signal_timer1_comp_b_isr(void)
 {
     uint32_t cur_ticks = timer1_32_get_ticks();
-    for (uint8_t i = 0; i < NMB_OF_OUTPUTS; i++)
+    for (uint8_t i = 0u; i < (NMB_OF_OUTPUTS-0); i++)
     {
-        if (channel_isr[i]->next_low_event <= cur_ticks)
+        if (((channel_isr[0]->next_low_event + channel_isr[0]->delay_ticks*i) <= cur_ticks) && (state[i] == 1u))
         {
             gpio_write(channel_isr[i]->pin_out.port,channel_isr[i]->pin_out.bit, 0u);
+            channel_isr[i]->next_hi_event  = cur_ticks + channel_isr[i]->period_ticks;
+            state[i] = 0u;
         }
     }
 
