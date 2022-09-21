fedb4b46e97c20691568190662e61b0abd9ed55e
Bonnski <bon@nestromo.com>
2022-09-21 22:28:58 +0200

    vscode buid target. i had to move the makefile in order to make it work

--- app.mk
+++ app.mk
@@ -1,11 +1,11 @@
 MCU = atmega328p
 PORT = COM11
 BAUDRATE = 115200
-MAKEFILE_DIR = ../generic
+MAKEFILE_DIR = target/generic
 
 MCU = ATmega328P
 TARGET = atmega328
-HAL_DIR = ../../atmega328p
+HAL_DIR = atmega328p
 APP_NAME = bldcdriver
 APP_CFG_DEFINE = $(shell echo $(APP_NAME) | tr a-z A-Z)
 
@@ -14,4 +14,4 @@ DEFINES = -D__AVR_ATmega328P__
 CFLAGS = -D F_CPU=16000000 -mmcu=atmega328p -D $(APP_CFG_DEFINE)
 
 
-include $(MAKEFILE_DIR)/makefile
+include C:\Users\Nestropex1\Documents\GIT\atmega328\target\generic/makefile
